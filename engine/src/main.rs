0f18375bf783b7d1b9b0c4f4e0ead2439db533b6
vvel0x <issa4700@gmail.com>
2022-08-22 21:54:42 +0100

    Updated DB keys to conincide with new structure

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -17,13 +17,12 @@ fn index() -> Result<Redirect, String> {
 #[get("/<slug>")]
 fn fetch_url(slug: &str) -> Result<Redirect, Redirect> {
    let mut conn = db::connect();
-   let key = format!("qta:{}", slug);
-   let hits_key = format!("qta:hits:{}", slug);
-   let fetch_link: Result<String, _> = conn.get(key);
+   let key = format!("quanta:{}", slug);
+   let fetch_link: Result<String, _> = conn.hget(&key, "url");
 
    match fetch_link {
     Ok(url) => Ok({
-        let _ : () = conn.incr(hits_key, 1).expect("Unable to increment counter!");
+        let _ : () = conn.hincr(&key, "hits", 1).expect("Unable to increment counter!");
         Redirect::temporary(url) // Using temporary redirects to allow counting hits
     }),
     _ => Err(Redirect::to("/")),
